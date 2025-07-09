d6fa7f7374a556686c7f34b306bab6e009021aa2
Gabriel <99690065+Grezende-gh@users.noreply.github.com>
2025-07-08 23:03:38 -0300

    Update exlista.py

--- exlista.py
+++ exlista.py
@@ -1,6 +1,4 @@
 # Exercício - Lista de tarefas com desfazer e refazer
-# Música para codar =)
-# Everybody wants to rule the world - Tears for fears
 # todo = [] -> lista de tarefas
 # todo = ['fazer café'] -> Adicionar fazer café
 # todo = ['fazer café', 'caminhar'] -> Adicionar caminhar
@@ -9,7 +7,6 @@
 # refazer = todo ['fazer café']
 # refazer = todo ['fazer café', 'caminhar']
 import os
-
 
 def listar(tarefas):
     print()
@@ -79,6 +76,7 @@ while True:
         comandos['adicionar']
     comando()
 
+    # Essa parte abaixo foi otimizada aqui acima, serve para entender como não ficar dependente do if, ja que ele pode alongar e prejudicar o código.
     # if tarefa == 'listar':
     #     listar(tarefas)
     #     continue
